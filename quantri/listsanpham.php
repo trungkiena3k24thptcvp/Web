cf61b6d7f3b858a3e79e25ab2cd396527cc54858
trungkiena3k24thptcvp <trungkienk1818@gmail.com>
2024-11-21 01:02:51 +0700

    Initial commit

---
+++ quantri/listsanpham.php
@@ -0,0 +1,14 @@
+<?php
+require('includes/header.php');
+?>
+
+
+<div> 
+    <h3>Trang liet ke san pham</h3>
+</div>
+    
+
+
+<?php
+require('includes/footer.php');
+?> 
---
+++ quantri/themsanpham.php
@@ -0,0 +1,14 @@
+<?php
+require('includes/header.php');
+?>
+
+
+<div> 
+    <h3>Them san pham</h3>
+</div>
+    
+
+
+<?php
+require('includes/footer.php');
+?> 
