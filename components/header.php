e9fc5d72d067955320aa1ab28d2a4a710d92dd33
trungkiena3k24thptcvp <trungkienk1818@gmail.com>
2024-12-01 17:51:59 +0700

    up

--- components/header.php
+++ components/header.php
@@ -232,12 +232,6 @@
      if ($is_homepage) {
         ?>
          <div class="hero__item set-bg" data-setbg="img/banner.jpeg">
-                        <div class="hero__text">
-                            <span>FRUIT FRESH</span>
-                            <h2>Vegetable <br />100% Organic</h2>
-                            <p>Free Pickup and Delivery Available</p>
-                            <a href="#" class="primary-btn">SHOP NOW</a>
-                        </div>
                     </div>
         <?php
      }
